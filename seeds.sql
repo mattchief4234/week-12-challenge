22b3a4ab40826e0b7461b6a5eb8971566d924025
mattchief4234 <mattchief4234@gmail.com>
2023-01-24 20:06:04 -0700

    wrote code for schema and seed

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,45 @@
 -- create the data for the departments - have at least 3--
+INSERT INTO departments (name)
+Values
+    ("Management"),
+    ("Marketing"),
+    ("Product Development"),
+    ("Accounting"),
+    ("Finance"),
+    ("Lawyers"),
+    ("Sanitation")
 
 --create the data for the roles at least 5 --
+INSERT INTO roles (title, salary, department)
+VALUES 
+    ("Manager", 100000, 1),
+    ("Assistant to the Manager", 30000, 1),
+    ("Sales lead", 70000, 2),
+    ("Sales Team Member", 35000, 2),
+    ("Lead Programmer", 85000, 3),
+    ("Programmer", 50000, 3),
+    ("Receptionist", 30000, 3),
+    ("lawyer", 80000, 6),
+    ("Accountant", 75000, 4)
+    ("Janitor", 55000, 7),
 
 -- create the data for employees have at least 10--
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Jason", "Borne", 1, 1),
+    ("Eddy", "Samson",3, 1),
+    ("Edd", "Fitzgerald",3, 2),
+    ("Ed", "Hill", 4, 2),
+    ("John", "Halo", 5, 1),
+    ("Carter", "A259", 6, 5),
+    ("Kat", "B320", 6, 5),
+    ("Emile", "A239", 6, 5),
+    ("Jun", "A266", 6, 5),
+    ("Jorge", "052", 6, 5),
+    ("Jessica", "Lamont", 7, 1),
+    ("Saul", "Goodman", 8, 1),
+    ("David", "Rodriguez",9, 1),
+    ("Rebecca", "Charles", 9, 1),
+    ("Emilano", "Cortez", 10, 1)
 
 -- then you export it OR you simply run the seed--
